df5f8644ec42fa03262a35bf58f806214b1100e9
Alex Olson <alexkellenolson@gmail.com>
2015-07-07 17:33:17 -0600

    initial dummy test

---
+++ test/index.js
@@ -0,0 +1,7 @@
+var should = require('chai').should();
+
+describe('Duo Security Admin API Node Client', function() {
+    it('should have at least one test', function() {
+        true.should.be.true;
+    });
+});
