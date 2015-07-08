dabc993b798fadbeae7b76efda566f8581beecd0
Alex Olson <alexkellenolson@gmail.com>
2015-07-08 10:30:26 -0600

    tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 var should = require('chai').should();
+var duo = require('../index');
 
 describe('Duo Security Admin API Node Client', function() {
-    it('should have at least one test', function() {
-        true.should.be.true;
+    it('should create an instance of itself', function() {
+        var client = new duo();
     });
 });
