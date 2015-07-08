fa94905fc6795013d13293b71683dbd83fa716c8
Alex Olson <alexkellenolson@gmail.com>
2015-07-08 17:33:02 -0600

    use minimist argument parser

--- test/index.js
+++ test/index.js
@@ -2,10 +2,32 @@
 
 var should = require('chai').should();
 var duo = require('../index');
+var async = require('async');
+var fs = require('fs');
 
 describe('Duo Security Admin API Node Client', function() {
+    before(function(done) {
+        async.waterfall([
+            function(cb) {
+                fs.readFile('duocreds.json', function(err, data) {
+                    cb(err, data);
+                });
+            }
+        ],
+        function(err, data) {
+            if (!err) {
+                this.creds = JSON.parse(data);
+                done();
+            } else {
+                throw new Error('Couldn\'t read duo credentials file.');
+            }
+        });
+    });
+
     it('should create an instance of itself', function() {
-        var client = new duo();
+        var client = new duo({
+                url: 'https://api-312a1404.duosecurity.com'
+            });
         client.should.be.an('object');
     });
 });
