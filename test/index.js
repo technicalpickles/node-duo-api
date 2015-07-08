8e62ab63c4cb85694ef2679053e14b445ffded75
Alex Olson <alexkellenolson@gmail.com>
2015-07-08 11:13:36 -0600

    building module structure

--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
+'use strict';
+
 var should = require('chai').should();
 var duo = require('../index');
 
 describe('Duo Security Admin API Node Client', function() {
     it('should create an instance of itself', function() {
-        var client = new duo();
+        var client = new duo({url: 'https://duosecurity.com'});
     });
 });
