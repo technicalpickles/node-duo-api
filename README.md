069a10aaf3935bb13cb8b93910187a808ed62b93
Alex Olson <alexkellenolson@gmail.com>
2015-07-22 11:27:02 -0600

    dont bother with setting DUO_API_USER environment variable. Changing the tests accordingly. User will be created & deleted as nodeclient->testuser.

--- README.md
+++ README.md
@@ -48,10 +48,6 @@ To run the tests on your local machine, create three environment variables:
 - `DUO_API_IKEY`: Duo api integration key.
 - `DUO_API_SKEY`: Duo api secret key.
 
-Optionally, you can also set the following additional environment variables:
-
-- `DUO_API_USER`: Valid duo username in your account. Setting this enables more rigorous testing of user lookup functionality.
-
 After setting the environment variables, run `gulp test`.
 
 ```bash
@@ -60,7 +56,6 @@ cd node-duo-api/
 export DUO_API_HOST=api-XXXXXXXX.duosecurity.com
 export DUO_API_IKEY=XXXXXXXXXXXXXXXXXXXX
 export DUO_API_SKEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-export DUO_API_USER=XXXXX # If you want to enable rigorous user lookup testing.
 npm install -g gulp # If you don't have gulp installed already.
 npm install # Install dependencies.
 gulp test
--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 var should = require('chai').should();
 var duo = require('../index');
 
+var TEST_USER = 'nodeclient->testuser';
 var TEST_USER_ID = '';
 
 describe('Duosecurity Node Client', function() {
@@ -29,28 +30,22 @@ describe('Duosecurity Node Client', function() {
                 });
             });
 
-            it('should retrieve information about a user', function() {
-                return this.client.request('get', '/admin/v1/users', {username: process.env.DUO_API_USER}).then(function(res) {
-                    res.stat.should.equal('OK');
-                    if (process.env.DUO_API_USER) {
-                        res.response.length.should.equal(1);
-                        res.response.shift().username.should.equal(process.env.DUO_API_USER);
-                    } else {
-                        res.response.should.be.empty;
-                    }
-                });
-            });
-
             it('should create a new user', function() {
-                return this.client.request('post', '/admin/v1/users', {username: 'node-duo-api-test-user'}).then(function(res) {
+                return this.client.request('post', '/admin/v1/users', {username: TEST_USER}).then(function(res) {
                     res.stat.should.equal('OK');
                     TEST_USER_ID = res.response.user_id;
                 })
             });
 
-            it('should enroll the new user');
+            it('should retrieve information about the new user', function() {
+                return this.client.request('get', '/admin/v1/users', {username: TEST_USER}).then(function(res) {
+                    res.stat.should.equal('OK');
+                    res.response.length.should.equal(1);
+                    res.response.shift().username.should.equal(TEST_USER);
+                });
+            });
 
-            it('should delete the enrolled user', function() {
+            it('should delete the new user', function() {
                 var path = [
                     '/admin/v1/users',
                     TEST_USER_ID
@@ -77,23 +72,16 @@ describe('Duosecurity Node Client', function() {
             });
 
             it('should retrieve information about a user', function(done) {
-                this.client.request('get', '/admin/v1/users', {username: process.env.DUO_API_USER}, function(error, res) {
+                this.client.request('get', '/admin/v1/users', {username: TEST_USER}, function(error, res) {
                     res.stat.should.equal('OK');
-                    if (process.env.DUO_API_USER) {
-                        res.response.length.should.equal(1);
-                        res.response.shift().username.should.equal(process.env.DUO_API_USER);
-                    } else {
-                        res.response.should.be.empty;
-                    }
+                    res.response.length.should.equal(1);
+                    res.response.shift().username.should.equal(TEST_USER);
                     done();
                 });
             });
 
             it('should create a new user');
-
-            it('should enroll the new user');
-
-            it('should delete the enrolled user');
+            it('should delete the new user');
 
         });
 
